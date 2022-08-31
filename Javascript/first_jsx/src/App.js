cc0a68d1ba6caddb1a2c19de4d6886e85ab85296
Trong Doan <doantrong8777@gmail.com>
2022-08-31 02:47:36 -0700

    componnents

--- Javascript/first_jsx/src/App.js
+++ Javascript/first_jsx/src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from 'react';
+import MyNewComponent from './components/MyNewComponent';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Dojo!</h1>
-      <h2>Things I need to do:</h2>
-      <ul>
-        <li>Learn React</li>
-        <li>Climb Mt.Everest</li>
-        <li>Run a marathon</li>
-        <li>Feed the dogs</li>
-      </ul>
+      <MyNewComponent firstName="Jane" lastName="Doe" age="45" hairColor="Black"/>
+      <MyNewComponent firstName="John" lastName="Smith" age="88" hairColor="Brown"/>
+      <MyNewComponent firstName="Millard" lastName="Fillmore" age="50" hairColor="Brown"/>
+      <MyNewComponent firstName="Maria" lastName="Smith" age="62" hairColor="Brown"/>
     </div>
   );
 }
